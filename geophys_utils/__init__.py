5fc77e12e6ab060e531b0462732d83670ebc1f94
Alex Ip <alex@trentham.net.au>
2016-11-17 10:10:15 +1100

    NetCDFLineUtils added to geophys_utils module

--- geophys_utils/__init__.py
+++ geophys_utils/__init__.py
@@ -6,5 +6,6 @@ Created on 16Nov.,2016
 from geophys_utils._array_pieces import array_pieces
 from geophys_utils._data_stats import DataStats
 from geophys_utils._netcdf_grid_utils import NetCDFGridUtils
+from geophys_utils._netcdf_line_utils import NetCDFLineUtils
 from geophys_utils._polygon_utils import get_grid_edge_points, get_netcdf_edge_points, points2convex_hull, points2alpha_shape, netcdf2convex_hull
 from geophys_utils._crs_utils import get_spatial_ref_from_crs, get_coordinate_transformation, get_utm_crs, transform_coords
