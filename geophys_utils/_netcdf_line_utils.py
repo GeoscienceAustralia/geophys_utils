c5d0cef22e563b68a64a4dcdc57fe9fb3bf6506f
Alex Ip <alex@trentham.net.au>
2016-11-22 17:08:34 +1100

    Destructor bug fixed

--- geophys_utils/_netcdf_line_utils.py
+++ geophys_utils/_netcdf_line_utils.py
@@ -131,7 +131,7 @@ class NetCDFLineUtils(object):
         '''
         NetCDFLineUtils Destructor
         '''
-        cache_file_path = self._nc_cache_dataset.filename()
+        cache_file_path = self._nc_cache_dataset.filepath()
         self._nc_cache_dataset.close()
         os.remove(cache_file_path)
         
