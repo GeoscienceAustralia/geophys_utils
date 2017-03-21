128dcb341d50f4a2b9b2e389d8581e9c1f70ce11
Alex Ip <alex@trentham.net.au>
2017-03-21 16:14:42 +1100

    Improved progress output

--- geophys_utils/_dem_utils.py
+++ geophys_utils/_dem_utils.py
@@ -277,19 +277,33 @@ class DEMUtils(NetCDFGridUtils):
                                   
             # Trim overlaps off source & destination slices
             source_slices = [slice(0 if dest_slices[dim_index].start < overlap else source_slices[dim_index].start+overlap, 
-                                  piece_array.shape[dim_index] if (self.data_variable.shape[dim_index] - dest_slices[dim_index].stop) < overlap else source_slices[dim_index].stop-overlap)
+                                   piece_array.shape[dim_index] if (self.data_variable.shape[dim_index] - dest_slices[dim_index].stop) < overlap else source_slices[dim_index].stop-overlap)
                              for dim_index in range(2)
                              ]
                                   
             dest_slices = [slice(0 if dest_slices[dim_index].start < overlap else dest_slices[dim_index].start+overlap, 
-                                  self.data_variable.shape[dim_index] if (self.data_variable.shape[dim_index] - dest_slices[dim_index].stop) < overlap else dest_slices[dim_index].stop-overlap)
+                                 self.data_variable.shape[dim_index] if (self.data_variable.shape[dim_index] - dest_slices[dim_index].stop) < overlap else dest_slices[dim_index].stop-overlap)
                            for dim_index in range(2)
                            ]
                                   
             # Write pieces to new datasets
+            print 'Writing %s slope array at %s' % (tuple([dest_slices[dim_index].stop - dest_slices[dim_index].start
+                                                     for dim_index in range(2)
+                                                     ]),
+                                              tuple([dest_slices[dim_index].start
+                                                     for dim_index in range(2)
+                                                     ])
+                                              )
             slope_variable[dest_slices] = slope_array[source_slices]  
             slope_nc_dataset.sync()   
                  
+            print 'Writing %s aspect array at %s' % (tuple([dest_slices[dim_index].stop - dest_slices[dim_index].start
+                                                     for dim_index in range(2)
+                                                     ]),
+                                              tuple([dest_slices[dim_index].start
+                                                     for dim_index in range(2)
+                                                     ])
+                                              )
             aspect_variable[dest_slices] = aspect_array[source_slices]      
             aspect_nc_dataset.sync()   
             
