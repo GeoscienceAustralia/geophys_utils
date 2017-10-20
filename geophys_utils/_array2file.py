46f597c71e96add08e92d1bbaef91f41f52261b5
Alex Ip <alex@trentham.net.au>
2017-10-20 12:52:32 +1100

    Minor enhancements

--- geophys_utils/_array2file.py
+++ geophys_utils/_array2file.py
@@ -5,14 +5,19 @@ Created on 20 Oct. 2017
 '''
 from osgeo import gdal
 
-def array2file(data_arrays, projection, geotransform, file_path, file_format, dtype=gdal.GDT_Float32):
+def array2file(data_arrays, 
+               projection, 
+               geotransform, 
+               file_path, 
+               file_format='GTiff', 
+               dtype=gdal.GDT_Float32):
     '''
     Function to output array to any(?) GDAL supported raster format and return GDAL dataset
     Formats defined in http://www.gdal.org/formats_list.html
     '''
     data_array_shape = data_arrays[0].shape
     assert [data_array.shape for data_array in data_arrays].count(data_array_shape) == len(data_arrays), 'Data arrays are of different shape'
-    print data_array_shape
+
     driver = gdal.GetDriverByName(file_format)
     gdal_dataset = driver.Create(file_path, 
                                  data_array_shape[1], data_array_shape[0], # Array must be ordered yx
@@ -21,6 +26,7 @@ def array2file(data_arrays, projection, geotransform, file_path, file_format, dt
     gdal_dataset.SetGeoTransform(geotransform)
     gdal_dataset.SetProjection(projection)
     
+    # Write arrays to file
     for band_index in range(len(data_arrays)):
         raster_band = gdal_dataset.GetRasterBand(band_index+1)
         raster_band.WriteArray(data_arrays[band_index])
