2d65001f77ebd447af38e9372e27c7ec78d397d5
Andrew Turner <andrew.turner@ga.gov.au>
2018-04-23 14:08:32 +1000

    String variables require dtype "S1"

--- geophys_utils/netcdf_converter/grav2netcdf_converter.py
+++ geophys_utils/netcdf_converter/grav2netcdf_converter.py
@@ -39,61 +39,113 @@ class Grav2NetCDFConverter(NetCDFConverter):
     CSV2NetCDFConverter concrete class for converting CSV data to netCDF
     '''
 
-    field_defs = [
-        {'short_name': 'obsno',
-         'long_name': 'observation number',
-         'column_name': 'obsno',
+    field_names = [
+        {'short_name': 'Obsno',
+         'long_name': 'Observation Number',
+         'database_field_name': 'obsno',
          'dtype': 'int8',
          },
-        {'short_name' : 'grav',
-         'long_name' : 'ground gravity',
-         'column_name' : 'grav',
+        {'short_name' : 'Grav',
+         'long_name' : 'Ground Gravity',
+         'database_field_name' : 'grav',
          'dtype' : 'float32',
          'units': 'um/s^2' #TODO: Confirm units in DB
         },
-        {'short_name': 'lat',
-         'long_name': 'latitude',
-         'column_name': 'dlat',
+        {'short_name': 'Lat',
+         'long_name': 'Latitude',
+         'database_field_name': 'dlat',
          'dtype': 'float',
          'units': 'degrees_north'
          },
-        {'short_name': 'long',
-         'long_name': 'longitude',
-         'column_name': 'dlong',
+        {'short_name': 'Long',
+         'long_name': 'Longitude',
+         'database_field_name': 'dlong',
          'dtype': 'float',
          'units': 'degrees_east'
          },
-        {'short_name': 'gndelev',
+        {'short_name': 'Gndelev',
          'long_name': 'Ground Elevation',
-         'column_name': 'GNDELEV',
+         'database_field_name': 'gndelev',
          'dtype': 'float32',
-         'units': 'metres'
+         'units': 'm'
          },
-        {'short_name': 'meterhgt',
-         'long_name': 'Meter Height',
-         'column_name': 'meterhgt',
+        {'short_name': 'Insthgt',
+         'long_name': 'Instrument Height', # changed meter height to instrument height - Meter hiegh in metres was confusing.
+         'database_field_name': 'meterhgt',
+         'table_name': '',
          'dtype': 'float32',
-         'units': 'metres'
+         'units': 'm'
          },
-        {'short_name': 'gridflag',
-         'long_name': """Gridding Flag: Flag to note whether data is included in gravity
-                        grids produced by GA.
-                        Flag = 1 indicated that the data was used in producing GA
-                        grids, such as the National Gravity Grids.
-                        Flag = 0: the data was not used in GA produced grids. This is
-                        usually because the data is less accurate. The data is included
-                        as it may still provide useful information.""",
-         'column_name': 'gridflag',
+        {'short_name': 'Gridflag',
+         'long_name': 'Grid Flag',
+         'database_field_name': 'gridflag',
          'dtype': 'int8',
+         'comment':
+             """
+            Gridding Flag: Flag to note whether data is included in gravity grids produced by GA.
+            Flag = 1 indicated that the data was used in producing GA grids, such as the National Gravity Grids.
+            Flag = 0: the data was not used in GA produced grids. This is usually because the data is less accurate.
+            The data is included as it may still provide useful information.""",
          },
-        {'short_name': 'reliability',
+
+        {'short_name': 'Reliab',
          'long_name': 'Estimation of Station Reliability',
-         'column_name': 'reliab',
+         'database_field_name': 'reliab',
          'dtype': 'int8',
-         }
+         'comment': """
+            0 Unreliable data which should not be used pending remedial action.
+            1 Insufficient information to accurately classify but still regarded as reliable data.
+            2 Poorly controlled data which should be used cautiously.
+            3 Data with weak gravity, position and elevation control.
+            4 Data with moderate gravity, position and elevation control.
+            5 Documented gravity ties, levelled elevations and accurately scaled positions.
+            6 A point occupied once with well-defined position and elevation.
+            7 Multiple occupations at a point with well-defined position and elevation.
+            8 Multiple measurements at a point with accurate position and elevation.
+            9 Data measured numerous times with absolute, geodetic or first order precision."""
+         },
+
+
+        {'short_name': 'stattype',
+         'long_name': 'Station Type',
+         'database_field_name': 'stationtype',
+         'dtype': 'S1',
+         'comment': """
+            A Absolute measurement.
+            B Base station - usually a Fundamental Gravity Network station.
+            C Control station tied to a B station or a cell centre in a survey.
+            D Detailed (spacing < 1km) survey or traverse station with optical levelling or GPS. Tie point or doubly read station in a survey.
+            E Elevation control point for a survey.
+            F Fill-in, grid, or semi-detailed survey with spacing > 1 and < 4km.
+            G Ground transport survey (4-6km).
+            H Helicopter transport survey (4-6km).
+            I Intermediate survey (7-9km).
+            J Spacing averaged out between and along traverses
+            M Marine transport survey.
+            Q A repeat station used for survey quality control.
+            R Regional selection from various surveys, not regularly used now
+            V Various, conglomeration of a number of surveys with varying spacing, observation dates, and/or station types
+            ? Unknown. Further checking is required"""},
+
+        # {'short_name': 'Locacc',
+        #  'long_name': 'Location Accuracy',
+        #  'database_field_name': 'LOCACC',
+        #  'dtype': 'char',
+        #  'units': 'degrees'}
     ]
 
-
+    gravity_metadata_list = [
+        'SURVEYID',
+        'STATEGROUP',
+        'STATIONS',
+        'GRAVACC',
+        #'GRAVDATUM',
+        # as varaible'GNDELEVACC'
+        'GNDELEVMETH',
+        # should be survey 'GNDELEVDATUM',
+        #'RELIAB',
+        #survey 'LAYOUT',
+    ]
 
     def __init__(self, nc_out_path, survey_id, con, netcdf_format='NETCDF4_CLASSIC'):
         '''
@@ -103,33 +155,34 @@ class Grav2NetCDFConverter(NetCDFConverter):
         '''
 
         def get_survey_metadata(survey_id):
-            sql_statement = '''select * from gravity.GRAVSURVEYS gs
-                         inner join a.surveys using(eno)
-                         where gs.surveyid = {0}
-                         and exists 
-            (select o1.* from gravity.OBSERVATIONS o1
-            left join gravity.OBSERVATIONS o2
-            on o1.surveyid = o2.surveyid
-            and (o1.entrydate > o2.entrydate
-            OR(o1.entrydate = o2.entrydate and o1.obsno > o2.obsno))
-            and o1.geodetic_datum = o2.geodetic_datum
-            and o1.dlat = o2.dlat
-            and o1.dlong = o2.dlong
-            and o1.access_code = o2.access_code
-            and o1.status = o2.status
-        where
-        o1.surveyid = {0}
-        and o1.status = 'A'
-        and o1.access_code = 'O'
-        and o2.obsno is null
-        and o1.grav is not null
-        and o1.gndelev is not null
-        and o1.meterhgt is not null
-        and o1.nvalue is not null
-        and o1.ellipsoidhgt is not null
-        and o1.ellipsoidmeterhgt is not null
-        and o1.eno in (select eno from a.surveys 
-        where countryid is null or countryid = 'AUS'))'''.format(self.survey_id)
+            sql_statement = '''
+            select * from gravity.GRAVSURVEYS gs
+                inner join a.surveys using(eno)
+                where gs.surveyid = {0}
+                and exists 
+                    (select o1.* from gravity.OBSERVATIONS o1
+                    left join gravity.OBSERVATIONS o2
+                    on o1.surveyid = o2.surveyid
+                    and (o1.entrydate > o2.entrydate OR(o1.entrydate = o2.entrydate and o1.obsno > o2.obsno))
+                    and o1.geodetic_datum = o2.geodetic_datum
+                    and o1.dlat = o2.dlat
+                    and o1.dlong = o2.dlong
+                    and o1.access_code = o2.access_code
+                    and o1.status = o2.status
+                        where
+                        o1.surveyid = {0}
+                        and o1.status = 'A'
+                        and o1.access_code = 'O'
+                        and o1.dlat is not null
+                        and o1.dlong is not null
+                        and o1.grav is not null
+                        and o1.gndelev is not null
+                        and o1.meterhgt is not null
+                        and o1.nvalue is not null
+                        and o1.ellipsoidhgt is not null
+                        and o1.ellipsoidmeterhgt is not null
+                        and o1.eno in (select eno from a.surveys where countryid is null or countryid = 'AUS')
+                        and o2.obsno is null)'''.format(self.survey_id)
 
             # sql_statement = '''select * from gravity.GRAVSURVEYS gs
             #              inner join a.surveys using(eno)
@@ -154,7 +207,6 @@ class Grav2NetCDFConverter(NetCDFConverter):
                             )
                         )
 
-
         def get_survey_metadata_in_obs_table(survey_id):
             columns_to_add = {'LOCCACCUOM' : None }
             for key, value in iter(columns_to_add.items()):
@@ -167,51 +219,40 @@ class Grav2NetCDFConverter(NetCDFConverter):
                 #                      and geodetic_datum = 'GDA94'
                 #                      )'''.format(key, survey_id)
 
-
-
                 query_result = self.cursor.execute(sql_statement)
                 value = next(query_result)
             return value
-
-
-
-
 
         NetCDFConverter.__init__(self, nc_out_path, netcdf_format)
 
         self.cursor = con.cursor()
         self.survey_id = survey_id
-
         self.survey_metadata = get_survey_metadata(survey_id)
-
-
-
 
     def get_global_attributes(self):
         '''
         Concrete method to return dict of global attribute <key>:<value> pairs
         '''
         print("LAT")
-        print(self.nc_output_dataset.variables['lat'])
+        print(self.nc_output_dataset.variables['Lat'])
         print("LONG")
-        print(self.nc_output_dataset.variables['long'])
+        print(self.nc_output_dataset.variables['Long'])
         # insert survey wide metadata
         metadata_dict = {'title': self.survey_metadata['SURVEYNAME'],
             'Conventions': "CF-1.6,ACDD-1.3",
             'Gravity_Accuracy' : self.survey_metadata['GRAVACC'], #example of how to add oracle fields to global attributes
-
             'featureType': "trajectory",
             'keywords': 'blah',
-            'geospatial_east_min': np.min(self.nc_output_dataset.variables['long']),
-            'geospatial_east_max': np.max(self.nc_output_dataset.variables['long']),
+            'geospatial_east_min': np.min(self.nc_output_dataset.variables['Long']),
+            'geospatial_east_max': np.max(self.nc_output_dataset.variables['Long']),
             'geospatial_east_units': "m",
             'geospatial_east_resolution': "point",
-            'geospatial_north_min': np.min(self.nc_output_dataset.variables['lat']),
-            'geospatial_north_max': np.max(self.nc_output_dataset.variables['lat']),
+            'geospatial_north_min': np.min(self.nc_output_dataset.variables['Lat']),
+            'geospatial_north_max': np.max(self.nc_output_dataset.variables['Lat']),
             'geospatial_north_units': "m",
             'geospatial_north_resolution': "point",
-            'geospatial_vertical_min': np.min(self.nc_output_dataset.variables[('gndelev')]), # should say if I use gndelev or meter height
-            'geospatial_vertical_max': np.max(self.nc_output_dataset.variables[('gndelev')]), # Should this be min(elevation-DOI)?
+            'geospatial_vertical_min': np.min(self.nc_output_dataset.variables[('Gndelev')]), # should say if I use gndelev or meter height
+            'geospatial_vertical_max': np.max(self.nc_output_dataset.variables[('Gndelev')]), # Should this be min(elevation-DOI)?
             'geospatial_vertical_units': "m",
             'geospatial_vertical_resolution': "point",
             'geospatial_vertical_positive': "up",
@@ -236,6 +277,7 @@ class Grav2NetCDFConverter(NetCDFConverter):
 # and access_code = 'O'
 # and geodetic_datum = 'GDA94' or geodetic_datum = 'WGS84'
 # """.format(self.survey_id)
+
         sql_statement = '''
                        select count(*) from gravity.OBSERVATIONS o1
                        left join gravity.OBSERVATIONS o2
@@ -313,7 +355,7 @@ class Grav2NetCDFConverter(NetCDFConverter):
                                and o1.ellipsoidmeterhgt is not null
                                and o1.eno in (select
                            eno from a.surveys where countryid is null or countryid = 'AUS')'''\
-                .format(field_def['column_name'], self.survey_id)
+                .format(field_def['database_field_name'], self.survey_id)
 
             #print(sql_statement)
             variable_list = []
@@ -321,12 +363,33 @@ class Grav2NetCDFConverter(NetCDFConverter):
             for i in self.cursor:
                 variable_list.append(
                     i[0])  # getting the first index is required. Otherwise each point is within its own tuple.
-            print("variable_list read from oracle")
-            print(variable_list)
-            new = np.array(variable_list, dtype=field_def['dtype'])
-            print(np.max(new))
-            return np.array(variable_list, dtype=field_def['dtype'])
 
+            # # if the field contains characters it must be handled differently
+            # if field_def['dtype'] is "char":
+            #     print(field_def['dtype'])
+            #     #return variable_list
+            #     print('variable lsit')
+            #     print(variable_list)
+            #     str_out = netCDF4.stringtochar(np.array(variable_list, 'S1'))
+            #     str_out2 = str_out.flatten('F')
+            #     print('str_out2 lsit')
+            #     print(str_out2)
+            #     print(str_out2.shape)
+            #     #b = np.array([list(word) for word in variable_list])
+            #
+            #     print(str_out.shape)
+            #     #return np.array(str_out, dtype=str_out.dtype.str)
+            #
+            #
+            #     return np.array(str_out2, dtype='S1')
+
+
+
+            return np.array(variable_list, dtype=field_def['dtype'])
+            # brah = np.char.array(variable_list, unicode=True)
+            # print(brah)
+            # print("ES")
+            # return np.char.array(variable_list, unicode=True)
         # crs variable creation for GDA94
         yield self.build_crs_variable('''\
         GEOGCS["GDA94",
@@ -347,6 +410,8 @@ class Grav2NetCDFConverter(NetCDFConverter):
                             else value
                             for key, value in iter(self.survey_metadata.items())
                             if value is not None
+                            #if value in Grav2NetCDFConverter.gravity_metadata_list
+
                             }
         #pprint(gravity_metadata)
         yield NetCDFVariable(short_name='ga_gravity_metadata',
@@ -358,9 +423,8 @@ class Grav2NetCDFConverter(NetCDFConverter):
                               )
 
 
-        for field_def in Grav2NetCDFConverter.field_defs:
-
-            list_of_possible_value = ['long_name', 'units']
+        for field_def in Grav2NetCDFConverter.field_names:
+            list_of_possible_value = ['long_name', 'units', 'dtype', 'comment']
             attributes_dict = {}
             for a in list_of_possible_value:
                 if field_def.get(a):
@@ -374,7 +438,6 @@ class Grav2NetCDFConverter(NetCDFConverter):
                                  dimensions=['point'],
                                  fill_value=-1,
                                  attributes=attributes_dict
-
                                  )
 
 
@@ -394,17 +457,16 @@ class Grav2NetCDFConverter(NetCDFConverter):
 
 
 def main():
+    # get user input and connect to oracle
     assert len(sys.argv) >= 4, '....'
     nc_out_path = sys.argv[1]
     u_id = sys.argv[2]
     oracle_database = sys.argv[3]
     pw = sys.argv[4]
     con = cx_Oracle.connect(u_id, pw, oracle_database)
-    print("HEELO")
     survey_cursor = con.cursor()
-    #sql_get_surveyids = "select Surveyid from gravity.GRAVSURVEYS"
-    # get the list of surveyids
 
+    # get a list of all survey ids
     sql_get_surveyids = """select Surveyid from gravity.GRAVSURVEYS gs
                         where exists (select * from gravity.OBSERVATIONS go
                         where go.surveyid = gs.surveyid
@@ -418,22 +480,19 @@ def main():
 
     survey_cursor.execute(sql_get_surveyids)
     survey_id_list = []
-    print("HERE?")
+
+    # tidy the survey id strings
     for survey_row in survey_cursor:
         tidy_sur = re.search('\d+', survey_row[0]).group()
         survey_id_list.append(tidy_sur)
-        print("TIDY")
-        print(tidy_sur)
 
-    count =1
-    print(survey_id_list)
+
+    #print(survey_id_list)
     print('Survey count =',len(survey_id_list))
+    # Loop throught he survey lists to make a netcdf file based off each one.
     for survey in survey_id_list:
         print(survey)
         g2n = Grav2NetCDFConverter(nc_out_path + str(survey) + '.nc', survey, con)
-        #print('netcdf dict')
-        #print(g2n.nc_output_dataset.__dict__)
-
         g2n.convert2netcdf()
         print('Finished writing netCDF file {}'.format(nc_out_path))
 
@@ -443,10 +502,10 @@ def main():
         print(g2n.nc_output_dataset.dimensions)
         print('Variables:')
         print(g2n.nc_output_dataset.variables)
+        print(g2n.nc_output_dataset.file_format)
+        print(g2n.nc_output_dataset.variables["stattype"][:])
         del g2n
         break
-
-                #count = count + 1
 
 
 if __name__ == '__main__':
