baea68e1ebf14eee1bbb381c60777dac45bd7ebd
Alex Ip <alex@trentham.net.au>
2018-07-19 17:43:20 +1000

    DDL bugs fixed

--- geophys_utils/dataset_metadata_cache/_postgres_dataset_metadata_cache.py
+++ geophys_utils/dataset_metadata_cache/_postgres_dataset_metadata_cache.py
@@ -80,98 +80,153 @@ class PostgresDatasetMetadataCache(DatasetMetadataCache):
         cursor.execute(sql)
 
 
-    def get_survey_id(self, 
+
+    def add_survey(self, 
                       ga_survey_id,
                       survey_name=None
                       ):
         '''
-        Function to return primary key of survey, inserting if necessary
+        Function to insert survey if necessary
         '''
+        if not ga_survey_id:
+            return None
+        
         cursor = self.db_connection.cursor()
         
         params = {'ga_survey_id': ga_survey_id,
                   'survey_name': survey_name
                   }
         
-        select_sql = '''select survey_id, ga_survey_id, survey_name from survey
-where ga_survey_id = %(ga_survey_id)s;
-'''
-        # Check for existing record
-        cursor.execute(select_sql, params)
-        try:
-            row = next(cursor)
-            params['survey_id'] = row[0]
-            
-            # Update details if required
-            if survey_name and row[2] != survey_name:
-                update_sql = '''update survey
-set survey_name = %(survey_name)s
-where survey_id = %(survey_id)s;
-'''
-                cursor.execute(update_sql, params)
-                
-            return params['survey_id'] 
-          
-        except StopIteration: # No existing record found       
-            insert_sql = '''insert into survey(ga_survey_id, survey_name)
+        insert_survey_sql = '''insert into survey(ga_survey_id, survey_name)
 values(%(ga_survey_id)s, %(survey_name)s)
 ON CONFLICT (ga_survey_id) DO NOTHING;
 '''            
-            cursor.execute(insert_sql, params)
-        
-            if cursor.rowcount:
-                logger.info('GA survey ID "{}" inserted into survey table for survey with name "{}"'.format(ga_survey_id, survey_name))
-                cursor.execute(select_sql, params)
-                row = next(cursor)
-                return row[0]            
-            else:
-                raise BaseException('Unable to insert new survey')
+        cursor.execute(insert_survey_sql, params)
+    
+        if cursor.rowcount:
+            logger.info('New survey "{}" inserted into table'.format(ga_survey_id))
+        else:
+            logger.debug('Survey "{}" already exists in table'.format(ga_survey_id))
             
             
-    def get_keyword_id(self, 
-                       keyword):
+    def add_keywords(self,
+                     dataset_id, 
+                     keyword_list):
         '''
         Function to return primary key of keyword, inserting if necessary
         '''
         cursor = self.db_connection.cursor()
         
-        params = {'keyword': keyword
-                  }
-        
-        select_sql = '''select * from keyword
-where keyword = %(keyword)s;
-'''
-        # Check for existing record
-        cursor.execute(select_sql, params)
-        try:
-            row = next(cursor)
-            return row[0]
-          
-        except StopIteration: # No existing record found       
-            insert_sql = '''insert into keyword(keyword)
+        # Try inserting keywords individually
+        for keyword in keyword_list:
+            params = {'dataset_id': dataset_id,
+                      'keyword': keyword
+                      }
+            
+            insert_keyword_sql = '''insert into keyword(keyword_value)
 values(%(keyword)s)
-ON CONFLICT (keyword) DO NOTHING;
+ON CONFLICT (keyword_value) DO NOTHING;
 '''            
-            cursor.execute(insert_sql, params)
+            cursor.execute(insert_keyword_sql, params)
         
             if cursor.rowcount:
-                logger.info('Keyword "{}" inserted into keyword table'.format(keyword))
-                cursor.execute(select_sql, params)
-                row = next(cursor)
-                return row[0]            
+                logger.info('New keyword "{}" inserted into table'.format(keyword))
             else:
-                raise BaseException('Unable to insert new keyword')
-            
-
-    def get_distribution_id(self, 
-                            distribution):
-        '''
-        Function to return primary key of distribution, inserting if necessary
-        '''
-
-    def get_protocol_id(self, 
-                        protocol_value):
-        '''
-        Function to return primary key of protocol, inserting if necessary
-        '''
+                logger.debug('Keyword "{}" already exists in table'.format(keyword))
+                
+                
+        # Insert dataset_keyword records in bulk    
+        params = {'dataset_id': dataset_id,
+                  'keywords': keyword_list
+                  }
         
+        insert_dataset_keyword_sql = '''insert into dataset_keyword(dataset_id, keyword_id)
+select %(dataset_id)s, 
+    keyword_id from keyword
+    where keyword_value in %(keywords)s
+ON CONFLICT (dataset_id, keyword_id) DO NOTHING;
+'''            
+        cursor.execute(insert_dataset_keyword_sql, params)
+        
+
+#===============================================================================
+#     def get_keyword_id(self, 
+#                        keyword):
+#         '''
+#         Function to return primary key of keyword, inserting if necessary
+#         '''
+#         cursor = self.db_connection.cursor()
+#         
+#         params = {'keyword': keyword
+#                   }
+#         
+#         select_sql = '''select * from keyword
+# where keyword = %(keyword)s;
+# '''
+#         # Check for existing record
+#         cursor.execute(select_sql, params)
+#         try:
+#             row = next(cursor)
+#             return row[0]
+#           
+#         except StopIteration: # No existing record found       
+#             insert_sql = '''insert into keyword(keyword)
+# values(%(keyword)s)
+# ON CONFLICT (keyword) DO NOTHING;
+# '''            
+#             cursor.execute(insert_sql, params)
+#         
+#             if cursor.rowcount:
+#                 logger.info('Keyword "{}" inserted into keyword table'.format(keyword))
+#                 cursor.execute(select_sql, params)
+#                 row = next(cursor)
+#                 return row[0]            
+#             else:
+#                 raise BaseException('Unable to insert new keyword')
+#             
+#===============================================================================
+
+    def add_distributions(self,
+                          dataset_id, 
+                          distribution_list):
+        '''
+        Function to insert new distribution
+        '''
+        cursor = self.db_connection.cursor()
+            
+        # Only add each protocol once
+        for protocol in set([distribution.protocol for distribution in distribution_list]):
+            params = {'protocol_name': protocol
+                      }
+            
+            insert_protocol_sql = '''insert into protocol(protocol_name)
+values(%(protocol_name)s)
+ON CONFLICT (protocol_name) DO NOTHING;
+'''            
+            cursor.execute(insert_protocol_sql, params)
+            
+            if cursor.rowcount:
+                logger.info('New protocol "{}" inserted into protocol table'.format(distribution.protocol))
+            else:
+                logger.debug('Protocol "{}" already exists in protocol table'.format(distribution.protocol))
+                
+            
+        for distribution in distribution_list:
+            params = {'dataset_id': dataset_id,
+                      'distribution_url': distribution.url,
+                      'protocol_name': distribution.protocol
+                      }
+    
+            insert_distribution_sql = '''insert into distribution(dataset_id, distribution_url, protocol_id)
+values(%(dataset_id)s, 
+    %(distribution_url)s
+    (select protocol_id from protocol where protocol_name = %(protocol_name)s)
+    )
+ON CONFLICT (dataset_id, distribution_url, protocol_id) DO NOTHING;
+'''            
+            cursor.execute(insert_distribution_sql, params)
+            
+            if cursor.rowcount:
+                logger.info('Distribution "{}" inserted into table'.format(distribution.url))
+            else:
+                logger.debug('Distribution "{}" already exists in table'.format(distribution.url))
