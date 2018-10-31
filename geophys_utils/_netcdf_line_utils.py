ba27f4cf87757fef68aa20763a9aee2b71bd2a10
Andrew Turner <andrew.turner@ga.gov.au>
2018-11-01 10:35:27 +1100

    cache path quick fix reversed.

--- geophys_utils/_netcdf_line_utils.py
+++ geophys_utils/_netcdf_line_utils.py
@@ -165,7 +165,6 @@ class NetCDFLineUtils(NetCDFPointUtils):
             if line_point_count: # This test should be redundant
                 # Use subset of indices if stride is set
                 if subsampling_distance:
-                    logger.debug(self.xycoords)
                     line_length = pdist([self.xycoords[point_indices[0]], self.xycoords[point_indices[-1]]])[0]
                     logger.debug('line_length: {}'.format(line_length))
                     stride = max(1, int(line_point_count/max(1, line_length/subsampling_distance)))
