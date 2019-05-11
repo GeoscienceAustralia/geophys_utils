7d9d148b770bba24305b8792158fa0de1396fc84
Alex Ip <alex@trentham.net.au>
2019-05-11 16:25:49 +1000

    Hillshade added to example 10

--- examples/dynamic_point_gridding_utils.py
+++ examples/dynamic_point_gridding_utils.py
@@ -193,3 +193,21 @@ def grid_points(coordinates,
                     ] 
 
     return grid_array, grid_wkt, geotransform
+
+
+def hillshade(array, azimuth, angle_altitude):
+    '''
+    Hillshade function adapted from:
+    http://geoexamples.blogspot.com/2014/03/shaded-relief-images-using-gdal-python.html
+    '''       
+    x, y = np.gradient(array)
+    slope = np.pi/2. - np.arctan(np.sqrt(x*x + y*y))
+    aspect = np.arctan2(-x, y)
+    azimuthrad = azimuth * np.pi / 180.
+    altituderad = angle_altitude * np.pi / 180.
+ 
+    shaded = np.sin(altituderad) * np.sin(slope)\
+     + np.cos(altituderad) * np.cos(slope)\
+     * np.cos(azimuthrad - aspect)
+    
+    return 255 * (shaded + 1) / 2    
