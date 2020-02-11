6fa9f043eb0722d952f9e4e97523ec0aae63097e
Alex Ip <alex@trentham.net.au>
2020-02-11 18:51:46 +1100

    Debug level set back to INFO

--- geophys_utils/_concave_hull.py
+++ geophys_utils/_concave_hull.py
@@ -11,7 +11,7 @@ from matplotlib.path import Path
 import logging
 
 logger = logging.getLogger(__name__)
-logger.level = logging.DEBUG
+logger.level = logging.INFO
 
 K_MULTIPLIER = 1.5 # Multiplier for k between iterations
 
