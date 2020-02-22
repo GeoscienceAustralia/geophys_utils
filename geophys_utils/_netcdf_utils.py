412655c6767239bbe858d15bf00bbeeb0e4035e4
Alex Ip <alex@trentham.net.au>
2020-02-23 07:21:18 +1100

    rechunk code cleaned up

--- geophys_utils/_netcdf_utils.py
+++ geophys_utils/_netcdf_utils.py
@@ -30,7 +30,6 @@ import argparse
 import re
 import sys
 import numpy as np
-from distutils.util import strtobool
 import logging
 
 from geophys_utils._crs_utils import transform_coords
@@ -608,69 +607,3 @@ class NetCDFUtils(object):
 
         
 
-def main():
-    '''
-    Main function for quick and dirty testing
-    '''
-    # Define command line arguments
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('-c', '--copy', 
-                        dest='do_copy', 
-                        action='store_const', 
-                        const=True, default=False,
-                        help='Copy netCDF files')
-    parser.add_argument("-f", "--format", help="NetCDF file format (one of 'NETCDF4', 'NETCDF4_CLASSIC', 'NETCDF3_CLASSIC', 'NETCDF3_64BIT_OFFSET' or 'NETCDF3_64BIT_DATA')",
-                        type=str, default='NETCDF4')
-    parser.add_argument("--chunkspec", help="comma-separated list of <dimension_name>/<chunk_size> specifications",
-                        type=str)
-    parser.add_argument("--complevel", help="Compression level for chunked variables as an integer 0-9. Default is 4",
-                        type=int, default=4)
-    parser.add_argument('-d', '--debug', action='store_const', const=True, default=False,
-                        help='output debug information. Default is no debug info')
-    parser.add_argument("input_path")
-    parser.add_argument("output_path")
-    
-    args = parser.parse_args()
-    
-    if args.do_copy:
-        if args.chunkspec:
-            chunk_spec = {dim_name: int(chunk_size) 
-                        for dim_name, chunk_size in [chunk_spec_string.strip().split('/') for chunk_spec_string in args.chunkspec.split(',')]}
-        else:
-            chunk_spec = None
-            
-    ncu = NetCDFUtils(args.input_path,
-                      debug=args.debug
-                      )   
-    
-    ncu.copy(args.output_path, 
-             #datatype_map_dict={},
-             # Compress all chunked variables
-             variable_options_dict={variable_name: {'chunksizes': [chunk_spec.get(dimension) 
-                                                                   for dimension in variable.dimensions
-                                                                   ],
-                                                    'zlib': bool(args.complevel),
-                                                    'complevel': args.complevel
-                                                    }
-                               for variable_name, variable in ncu.netcdf_dataset.variables.items()
-                               if (set(variable.dimensions) & set(chunk_spec.keys()))
-                               } if chunk_spec else {},
-             #dim_range_dict={},
-             nc_format=args.format,
-             #limit_dim_size=False
-             )
-        
-
-if __name__ == '__main__':
-    console_handler = logging.StreamHandler(sys.stdout)
-    # console_handler.setLevel(logging.INFO)
-    console_handler.setLevel(logging.DEBUG)
-    console_formatter = logging.Formatter('%(name)s: %(message)s')
-    console_handler.setFormatter(console_formatter)
- 
-    if not logger.handlers:
-        logger.addHandler(console_handler)
-        logger.debug('Logging handlers set up for {}'.format(logger.name))
-
-    main()
