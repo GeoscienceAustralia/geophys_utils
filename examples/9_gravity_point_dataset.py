b091c13b5eacbf8853dbccf957be4d3bf7023792
Alex Ip <Alex@trentham.net.au>
2023-09-16 16:25:03 +1000

    Example notebooks 9 & 9a updated.

--- examples/9_gravity_point_dataset.py
+++ examples/9_gravity_point_dataset.py
@@ -1,6 +1,27 @@
+#!/usr/bin/env python
+
+# ===============================================================================
+#    Copyright 2017 Geoscience Australia
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+# ===============================================================================
+'''
+Created on 18Jun.,2018
+
+@author: Andrew Turner & Alex Ip
+'''
+import sys
 from math import log10, floor, pow
-from os import listdir
-from os.path import isfile, join
 
 import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
@@ -8,36 +29,51 @@ import matplotlib.pyplot as plt
 import netCDF4
 import numpy as np
 
-from geophys_utils import NetCDFPointUtils, get_spatial_ref_from_wkt
+from geophys_utils import NetCDFPointUtils
+from geophys_utils import get_spatial_ref_from_wkt
 
 
-# print(sys.version)
-# pprint(dict(os.environ))
-
-def rescale_array(input_np_array, new_range_min=0, new_range_max=1):
-    old_min = input_np_array.min()
-    old_range = input_np_array.max() - old_min
-    new_range = new_range_max - new_range_min
-
-    scaled_np_array = ((input_np_array - old_min) / old_range * new_range) + new_range_min
-
-    return scaled_np_array
-
-
-def plot_survey_points(netcdf_point_utils,  # NetCDFPointUtils object wrapping a netCDF dataset
-                       variable_to_map,  # String specifying variable name for colour map
-                       utm_bbox=None,  # utm_bbox is of form [xmin, ymin, xmax, ymax]
-                       colour_scheme='binary',  # Colour map
-                       point_size=10  # Size of point in plot
+def plot_point_dataset(netcdf_point_utils,
+                       variable_to_map,
+                       utm_bbox=None,
+                       plot_title=None,
+                       colour_scheme='binary',
+                       point_size=10,
+                       point_step=1
                        ):
-    utm_wkt, utm_coords = netcdf_point_utils.utm_coords(netcdf_point_utils.xycoords[:])
+    '''
+    Function to plot data points on a map
+    @param netcdf_point_utils: NetCDFPointUtils object wrapping a netCDF dataset
+    @param variable_to_map: String specifying variable name for colour map
+    @param utm_bbox: UTM Bounding box of form [xmin, ymin, xmax, ymax] or None for all points. Default=None
+    @param plot_title: String to prefix before dataset title. Default=None for dataset title or dataset basename
+    @param colour_scheme: String specifying colour scheme for data points. Default='binary'
+    @param point_size: Point size for data points. Default=10
+    @param point_step: Point step between plotted points - used to skip points in dense datasets. Default=1
+    '''
+
+    def rescale_array(input_np_array, new_range_min=0, new_range_max=1):
+        old_min = input_np_array.min()
+        old_range = input_np_array.max() - old_min
+        new_range = new_range_max - new_range_min
+
+        scaled_np_array = ((input_np_array - old_min) / old_range * new_range) + new_range_min
+
+        return scaled_np_array
+
+    if plot_title is None:
+        if hasattr(netcdf_point_utils.netcdf_dataset, 'title'):
+            plot_title = netcdf_point_utils.netcdf_dataset.title
+        else:
+            plot_title = netcdf_point_utils.netcdf_dataset.filepath()
+
+    utm_wkt, utm_coords = netcdf_point_utils.utm_coords(netcdf_point_utils.xycoords)
 
     utm_zone = get_spatial_ref_from_wkt(utm_wkt).GetUTMZone()  # -ve for Southern Hemisphere
     southern_hemisphere = (utm_zone < 0)
     utm_zone = abs(utm_zone)
     projection = ccrs.UTM(zone=utm_zone, southern_hemisphere=southern_hemisphere)
     print('utm_zone = {}'.format(utm_zone))
-    # print(utm_coords)
 
     variable = netcdf_point_utils.netcdf_dataset.variables[variable_to_map]
 
@@ -58,45 +94,44 @@ def plot_survey_points(netcdf_point_utils,  # NetCDFPointUtils object wrapping a
                                       )
         utm_coords = utm_coords[spatial_mask]
 
-    print('UTM bounding box: {}'.format(utm_bbox))
-    # print(utm_coords)
-    print(netcdf_point_utils.dimensions['point'])
+    print('{} points in UTM bounding box: {}'.format(np.count_nonzero(spatial_mask), utm_bbox))
+
     colour_array = rescale_array(variable[spatial_mask], 0, 1)
 
-    # map_image = cimgt.OSM() # https://www.openstreetmap.org/about
-    # map_image = cimgt.StamenTerrain() # http://maps.stamen.com/
-    map_image = cimgt.QuadtreeTiles()
     fig = plt.figure(figsize=(30, 30))
 
     ax = fig.add_subplot(1, 1, 1, projection=projection)
 
-    ax.set_title("Point Gravity Survey - " + str(netcdf_point_utils.netcdf_dataset.getncattr('title')))
+    ax.set_title(plot_title)
+
+    # map_image = cimgt.OSM() # https://www.openstreetmap.org/about
+    # map_image = cimgt.StamenTerrain() # http://maps.stamen.com/
+    map_image = cimgt.GoogleTiles(style='satellite')
 
     ax.add_image(map_image, 10)
-    print(utm_bbox)
+
     # Compute and set regular tick spacing
     range_x = utm_bbox[2] - utm_bbox[0]
     range_y = utm_bbox[3] - utm_bbox[1]
-    print("range_x: ".format(range_x))
     x_increment = pow(10.0, floor(log10(range_x))) / 2
     y_increment = pow(10.0, floor(log10(range_y))) / 2
     x_ticks = np.arange((utm_bbox[0] // x_increment + 1) * x_increment, utm_bbox[2], x_increment)
     y_ticks = np.arange((utm_bbox[1] // y_increment + 1) * y_increment, utm_bbox[3], y_increment)
-    ax.set_xticks(x_ticks)
-    ax.set_yticks(y_ticks)
+    plt.xticks(x_ticks, rotation=45)
+    plt.yticks(y_ticks)
 
     # set the x and y axis labels
-    ax.set_xlabel("Eastings (m)", rotation=0, labelpad=20)
-    ax.set_ylabel("Northings (m)", rotation=90, labelpad=20)
+    plt.xlabel("Eastings (m)", rotation=0, labelpad=20)
+    plt.ylabel("Northings (m)", rotation=90, labelpad=20)
 
     # See link for possible colourmap schemes: https://matplotlib.org/examples/color/colormaps_reference.html
     cm = plt.cm.get_cmap(colour_scheme)
 
     # build a scatter plot of the specified data, define marker, spatial reference system, and the chosen colour map type
-    sc = ax.scatter(utm_coords[:, 0],
-                    utm_coords[:, 1],
+    sc = ax.scatter(utm_coords[::point_step, 0],
+                    utm_coords[::point_step, 1],
                     marker='o',
-                    c=colour_array,
+                    c=colour_array[::point_step],
                     s=point_size,
                     alpha=0.9,
                     transform=projection,
@@ -104,36 +139,38 @@ def plot_survey_points(netcdf_point_utils,  # NetCDFPointUtils object wrapping a
                     )
 
     # set the colour bar ticks and labels
-    cb = plt.colorbar(sc, ticks=[0, 1])
-    cb.ax.set_yticklabels([str(np.min(variable)), str(np.max(variable))])  # vertically oriented colorbar
-    cb.set_label("{} {}".format(variable.long_name, variable.units))
+    try:  # not all variables have units. These will fail on the try and produce the map without tick labels.
+        cb = plt.colorbar(sc, ticks=[0, 1])
+        cb.ax.set_yticklabels(
+            [str(np.min(variable[spatial_mask])), str(np.max(variable[spatial_mask]))])  # vertically oriented colorbar
+        cb.set_label("{} {}".format(variable.long_name, variable.units))
+    except:
+        pass
+    print("show")
+    plt.show()
 
-    # plt.show()
-    plt.savefig("C:\\Users\\u62231\\Desktop\\GravityWork\\maps\\{}".format(
-        netcdf_point_utils.netcdf_dataset.getncattr('title')))
 
+def main():
+    '''
+    main function for quick and dirty testing
+    '''
+    nc_path = sys.argv[1]
+    variable_to_plot = sys.argv[2]
 
-# Create NetCDFPointUtils object for specified netCDF dataset
-# netcdf_path = 'http://dapds00.nci.org.au/thredds/dodsC/uc0/rr2_dev/axi547/ground_gravity/point_datasets/201780.nc'
-
-onlyfiles = [f for f in listdir("C:\\Users\\u62231\\Desktop\\grav_netcdfs_5") if
-             isfile(join("C:\\Users\\u62231\\Desktop\\grav_netcdfs_5", f))]
-for file in onlyfiles:
-    print(file)
-    netcdf_path = "C:\\Users\\u62231\\Desktop\\grav_netcdfs_5\\{}".format(file)
-    print(netcdf_path)
-
-    netcdf_dataset = netCDF4.Dataset(netcdf_path)
+    netcdf_dataset = netCDF4.Dataset(nc_path)
     npu = NetCDFPointUtils(netcdf_dataset)
 
-    # Plot entire survey
-    plot_survey_points(npu,
-                       'Bouguer',
-                       colour_scheme='gist_heat', point_size=10)
+    print('1D Point variables:\n\t{}'.format('\n\t'.join([key for key, value in netcdf_dataset.variables.items()
+                                                          if value.dimensions == ('point',)])))
+    # Plot spatial subset
+    plot_point_dataset(npu,
+                       variable_to_plot,
+                       # utm_bbox=[660000,7080000,680000,7330000],
+                       colour_scheme='gist_heat',
+                       point_size=30,
+                       point_step=100
+                       )
 
-# # Plot spatial subset
-# plot_survey_points(npu,
-#                    'Bouguer',
-#                    utm_bbox=[630000,7980000,680000,8030000],
-#                    colour_scheme='gist_heat',
-#                    point_size=50)
+
+if __name__ == '__main__':
+    main()
